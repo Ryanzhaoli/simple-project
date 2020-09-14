224c77e114fd21fe9dff8b2be6e3627f9c7be2a8
Ryan <jijizaza999@gmail.com>
2020-09-13 19:20:19 -0700

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+
+
+      const srY = ScrollReveal({
+        origin:'top',
+        distance:'100px',
+        duration:3000,
+        reset:true
+        });
+        
+        srY.reveal('.img-about',{delay:500});
+        srY.reveal('form',{delay:200});
+        srY.reveal('.tile',{delay:200});
+       
+     
+    
+      const srX =  ScrollReveal({
+            origin:'left',
+            distance:'100px',
+            duration:3000,
+            reset:true
+            })
+
+        srX.reveal('.section-title',{});
+        srX.reveal('.img-fluid',{delay:200});
+        srX.reveal('.skill',{delay:200});
+        srX.reveal('p',{delay:100});
+     
+   
+   
+
